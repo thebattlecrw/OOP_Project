1d7c51d82910bc13fc914e59b30b586983b4d484
33saba33 <saido372@gmail.com>
2024-11-26 13:57:52 +0100

    Update Hero.

--- src/game/Hero.java
+++ src/game/Hero.java
@@ -1,15 +1,45 @@
+// --- Import. ---
+import java.util.ArrayList;
+
+// --- Principal class. ---
 public class Hero {
-    private int gold;
 
-    public Hero() {
-        this.gold = 0;
+    // --- Attributs. ---
+    private String name;
+    private int healthPoints;
+    private int ammoCount;
+    private List<Key> keys;
+
+    // --- Getters. ---
+    public String getName() {
+        return name;
+    }
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+    public int getAmmoCount() {
+        return ammoCount;
+    }
+    public List<Key> getKeys() {
+        return keys;
     }
 
-    public int getGold() {
-        return gold;
+    // --- Setters. --- 
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+    public void setAmmoCount(int ammoCount) {
+        this.ammoCount = ammoCount;
     }
 
-    public void addGold(int amount) {
-        this.gold += amount;
+    // --- Constructors. ---
+    public Hero(){
+        this.name = "Generic";
+        this.healthPoints = 100;
+        this.ammoCount = 0;
+        this.keys = new ArrayList<>();
     }
 }
