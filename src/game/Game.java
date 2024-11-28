1da58e285701455ec87c0244f148ed982e5b05f0
Flechetas <Antospy-79@hotmail.fr>
2024-11-28 05:36:36 +0100

    RP + new rooms

--- src/game/Game.java
+++ src/game/Game.java
@@ -9,31 +9,41 @@ public class Game {
 
     public Game() {
         // Initialisation des salles
-        Room room1 = new Room("Salle de départ", "C'est la salle de départ.");
-        Room room2 = new Room("Salle 2", "Une salle avec un pot.");
-        Room room3 = new Room("Salle 3", "Une autre salle avec un pot.");
-        Room room4 = new Room("Salle 4", "Une salle vide.");
-        Room room5 = new Room("Salle 5", "Une autre salle vide.");
+
+        Room couloir3 = new Room("Couloir du 3ème étage", "Un couloir tout à fait banal. Il mène aux pièces «301» à «306», ainsi qu'au couloir du 2ème étage «couloir2».");
+
+        Room room301 = new Room("Salle 301", "C'est votre chambre. Il n'y a qu'une porte, qui mène au couloir du 3ème étage «Couloir3».");
+        Room room302 = new Room("Salle 302", "Une salle où un échange de tirs a eu lieu.");
+        Room room303 = new Room("Salle 303", "Une autre salle avec un trou.");
+        Room room304 = new Room("Salle 304", "Une salle où un échange de tirs a eu lieu.");
+        Room room305 = new Room("Salle 305 - Cabinet du Dr. Hou", "Une autre salle vide. Une inscription à moitié effacée, ou simplement mal écrite, est sur le tableau : «H LP 101 3__4");
+        Room room306 = new Room("Salle 306", "Une pièce avec un sergent agonisant. «S'il te plaît, dis moi que mon chef de compagnie va bien. Il allait se faire vacciner en salle 204.»");
 
         // Relier les salles entre elles
-        room1.setExit("east", room2);
-        room2.setExit("west", room1);
-        room2.setExit("east", room3);
-        room3.setExit("west", room2);
-        room3.setExit("east", room4);
-        room4.setExit("west", room3);
-        room4.setExit("east", room5);
-        room5.setExit("west", room4);
+        couloir3.setExit("301", room301);
+        couloir3.setExit("302", room302);
+        couloir3.setExit("303", room303);
+        couloir3.setExit("304", room304);
+        couloir3.setExit("305", room305);
+        couloir3.setExit("306", room306);
+
+        room301.setExit("couloir3", couloir3);
+        room302.setExit("couloir3", couloir3);
+        room303.setExit("couloir3", couloir3);
+        room304.setExit("couloir3", couloir3);
+        room305.setExit("couloir3", couloir3);
+        room306.setExit("couloir3", couloir3);
 
         // Ajouter les munitions
         Ammo ammo = new Ammo("Ammo");
-        room1.addItem(ammo);
+        room302.addItem(ammo);
+        room304.addItem(ammo);
 
         // Initialiser le héros
         hero = new Hero();
 
         // Commencer le jeu dans la salle de départ
-        currentRoom = room1;
+        currentRoom = room301;
     }
 
     public void start() {
@@ -41,10 +51,17 @@ public class Game {
         String input;
         boolean gameRunning = true;
 
+        System.out.println("""
+                _____________________________________________________________________________________________________________________________________________________________
+                Vous vous réveillez dans un hôpital militaire. Alors que vous ne deviez venir que pour un banal rappel de vaccin, le scénario tourne au cauchemar...
+                Le dit vaccin a été remplacé par un poison transformant les patients en zombis. Heureusement, votre corps n'a pas réagi comme attendu, mais ce n'est pas le
+                cas pour le reste du régiment... À vous de trouver une solution pour sortir d'ici, mais attention, certains ont déjà entammé le processus de transformation !
+                _____________________________________________________________________________________________________________________________________________________________""");
+
         while (gameRunning) {
-            System.out.println("\nVous êtes dans " + currentRoom.getName());
+            System.out.println("\n\n[ " + currentRoom.getName() + " ]");
             System.out.println(currentRoom.getDescription());
-            System.out.println("Que voulez-vous faire? (HELP pour la liste des commandes.)");
+            System.out.println("Que voulez-vous faire ? (HELP pour la liste des commandes.)");
             input = scanner.nextLine();
 
             if (input.startsWith("GO ")) {
